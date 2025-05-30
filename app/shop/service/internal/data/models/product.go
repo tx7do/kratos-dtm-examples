36a94658f341237ff23ab580fd8cede1601a7376
Bobo <yanglinbo@gmail.com>
2025-05-30 22:30:42 +0800

    feat: refactor.

--- app/shop/service/internal/data/models/product.go
+++ app/shop/service/internal/data/models/product.go
@@ -8,9 +8,9 @@ import (
 type Product struct {
 	gorm.Model
 
-	Name        *string        `gorm:"column:name"`
-	Description sql.NullString `gorm:"column:description"`
-	Stock       int32          `gorm:"column:stock"`
+	Name        *string        `gorm:"column:name; comment:商品名称"`
+	Description sql.NullString `gorm:"column:description; comment:商品描述"`
+	Stock       int32          `gorm:"column:stock; comment:商品库存"`
 }
 
 func (u Product) TableName() string {
